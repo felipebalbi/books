5bdcfb6fc92b5f58a8d587660bca5ba3f85b643c
Felipe Balbi <felipe@balbi.sh>
2021-06-05 20:50:37 +0300

    sicp: more ch2
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- structure-and-interpretation-of-computer-programs/ch2.scm
+++ structure-and-interpretation-of-computer-programs/ch2.scm
@@ -39,3 +39,43 @@
   (print-rat (make-rat -1 2))
   (print-rat (make-rat 1 -2))
   (print-rat (make-rat -1 -2)))
+
+(define (make-segment p q) (cons p q))
+(define (start-segment s) (car s))
+(define (end-segment s) (cdr s))
+
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (midpoint-segment s)
+  (let ((start-x (x-point (start-segment s)))
+	(start-y (y-point (start-segment s)))
+	(end-x (x-point (end-segment s)))
+	(end-y (y-point (end-segment s))))
+    (make-point
+     (average start-x end-x)
+     (average start-y end-y))))
+
+(define (print-point p)
+  (display "(")
+  (display (x-point p))
+  (display ", ")
+  (display (y-point p))
+  (display ")")
+  (newline))
+
+(define p1 (make-point 2 2))
+(define p2 (make-point 4 4))
+
+(define segment (make-segment p1 p2))
+
+(define midpoint (midpoint-segment segment))
+
+(define (ex2.2)
+  (print-point midpoint))
+
+
