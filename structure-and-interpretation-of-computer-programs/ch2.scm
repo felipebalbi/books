ca62edce8580cdbb95de198ea28b200afdcb25e4
Felipe Balbi <felipe@balbi.sh>
2021-06-08 18:42:09 +0300

    sicp: more ch2
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- structure-and-interpretation-of-computer-programs/ch2.scm
+++ structure-and-interpretation-of-computer-programs/ch2.scm
@@ -433,3 +433,30 @@
   (display (same-parity 2 3 4 5 6 7 8))
   (newline))
 
+(define (square n)
+  (* n n))
+
+(define (square-list items)
+  (if (null? items)
+      '()
+      (cons (square (car items))
+	    (square-list (cdr items)))))
+
+(define (square-list items)
+  (map square items))
+
+(define (ex2.21)
+  (display (square-list (list 1 2 3 4)))
+  (newline))
+
+(define (for-each proc items)
+  (if (null? items)
+      #t
+      (and (proc (car items)) (for-each proc (cdr items)))))
+
+(define (ex2.23)
+  (for-each
+   (lambda (x)
+     (display x)
+     (newline))
+   (list 1 2 3 4)))
