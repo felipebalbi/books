f4b1dde012d02186ddb4133788605c38bce38151
Felipe Balbi <felipe@balbi.sh>
2021-06-09 14:26:58 +0300

    sicp: more ch2
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- structure-and-interpretation-of-computer-programs/ch2.scm
+++ structure-and-interpretation-of-computer-programs/ch2.scm
@@ -632,3 +632,77 @@
 (define (ex2.32)
   (display (subsets '(1 2 3 4)))
   (newline))
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+	((not (pair? tree))
+	 (if (odd? tree) (square tree) 0))
+	(else (+ (sum-odd-squares (car tree))
+		 (sum-odd-squares (cdr tree))))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) '())
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (filter predicate (cdr sequence))))
+	(else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (list tree))
+	(else (append (enumerate-tree (car tree))
+		      (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+  (accumulate +
+	      0
+	      (map square
+		   (filter odd?
+			   (enumerate-tree tree)))))
+
+(define (fib n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+	  ((even? count)
+	   (fib-iter a
+		     b
+		     (+ (square p) (square q))
+		     (+ (* 2 p q) (square q))
+		     (/ count 2)))
+	  (else (fib-iter (+ (* b q) (* a q) (* a p))
+			  (+ (* b p) (* a q))
+			  p
+			  q
+			  (- count 1)))))
+  (fib-iter 1 0 0 1 n))
+
+(define (even-fibs n)
+  (accumulate cons
+	       '()
+	       (filter even?
+		       (map fib
+			    (enumerate-interval 0 n)))))
+
+(define (list-fib-squares n)
+  (accumulate cons
+	      '()
+	      (map square
+		   (map fib
+			(enumerate-interval 0 n)))))
+
+(define (product-of-squares-of-odd-elements sequence)
+  (accumulate *
+	      1
+	      (map square
+		   (filter odd? sequence))))
